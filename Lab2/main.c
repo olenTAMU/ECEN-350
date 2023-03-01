c8e6edc4ff9cd7781c7e06dce82e0d1a75d3e97e
olenTAMU <90735414+olenTAMU@users.noreply.github.com>
2023-03-01 15:32:22 -0500

    Add files via upload

---
+++ Lab2/main.c
@@ -0,0 +1,14 @@
+extern long long int test();
+extern long long int lab02b();
+extern void lab02c(long long int a);
+extern long long int lab02d(long long int a);
+
+int main(void)
+{
+	test();
+	lab02b();
+	lab02c(233);
+	long long int a = lab02d(100);
+	
+    return 0;
+}
